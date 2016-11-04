6f149e431461b92d5e9280898e4617684e88a181
zuibuxing <493409245@qq.com>
2016-11-04 12:11:18 +0800

    add  function minus

---
+++ Project0001-Module0001/src/main/java/com/zuibuxing/utils/MathUtil.java
@@ -0,0 +1,18 @@
+package com.zuibuxing.utils;
+
+/**
+ * Created by HT on 2016/11/3.
+ */
+public class MathUtil {
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static int minus(int a,int b){ return a-b;}
+
+    public static void main(String args[]){
+
+        System.out.print("3 + 9 = "+add(3,9));
+    }
+}
